2dd7c87184948a0d7e6e538a0bb7cbb3554485ee
lazizB17 <abdulazizboriyev993@gmail.com>
2022-06-06 21:38:54 +0500

    commit 2

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -39,16 +39,28 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
+                  // #Welcome to Uno To Do
                   Container(
                     width: 200,
-                    height: 200,
+                    height: 90,
                     child: Text(
                       "Welcome to\nUno To Do",
                       style: Themes.textStyle1(),
                       textAlign: TextAlign.center,
                     ),
                   ),
-                  const SizedBox(height: 16,),
+                  // #Start using the best To Do app
+                  Container(
+                    margin: const EdgeInsets.only(bottom: 50),
+                    width: 240,
+                    height: 20,
+                    child: const Text(
+                      "Start using the best To Do app",
+                      style: TextStyle(color: Color(0xFF5835E5), fontSize: 16),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                  // #Get started
                   MaterialButton(
                     minWidth: MediaQuery.of(context).size.width - 60,
                     height: 50,
